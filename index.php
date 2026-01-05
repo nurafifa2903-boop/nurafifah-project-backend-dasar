153f7966daf5974dbd80b729297b87152d0a08a9
nurafifa2903-boop <nurafifa2903@gmail.com>
2026-01-05 21:31:05 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
-<?php
-phpinfo();
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar API PHP</title>
+</head>
+<body>
+    <h1>Message Form</h1>
+    <form action="submit.php"method="POST">
+  Nama:
+  <input type="text" name="nama"><br><br>
+
+  Pesan:
+  <textarea name="pesan"></textarea><br><br>
+
+  <button type="submit" name="kirim">Kirim</button>
+</form>
+        
+    </form>
+</body>
+</html>
